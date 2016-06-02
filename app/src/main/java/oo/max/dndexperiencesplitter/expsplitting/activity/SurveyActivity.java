d75fedcd95c36e7160aa6e3d45304a696429e344
Max <maksymilian.jaglak@gmail.com>
2016-06-02 21:45:17 +0200

    adding possibility to lock assessment for current player in SurveyActivity

--- app/src/main/java/oo/max/dndexperiencesplitter/expsplitting/activity/SurveyActivity.java
+++ app/src/main/java/oo/max/dndexperiencesplitter/expsplitting/activity/SurveyActivity.java
@@ -8,9 +8,13 @@ import android.support.v7.app.AlertDialog;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 import javax.inject.Inject;
 
@@ -39,6 +43,12 @@ public class SurveyActivity extends AbstractBaseActivity implements LoadPlayersA
     @Bind(R.id.assess_recycler_view)
     RecyclerView assessPlayersRecyclerView;
 
+    @Bind(R.id.lock_player_assessment)
+    Button lockAssessmentButton;
+
+    @Bind(R.id.assessment_locked_container)
+    LinearLayout assessmentLockedTextContainer;
+
     private PickingRequest pickingRequest;
 
     @Inject
@@ -49,6 +59,9 @@ public class SurveyActivity extends AbstractBaseActivity implements LoadPlayersA
 
     private List<Player> players;
     private List<Category> categories;
+    private Player currentlyVisiblePlayer;
+
+    private Set<Player> lockedPlayers = new HashSet<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -94,6 +107,15 @@ public class SurveyActivity extends AbstractBaseActivity implements LoadPlayersA
     }
 
     private void loadPlayersSurvey(Player pickedPlayer) {
+        currentlyVisiblePlayer = pickedPlayer;
+
+        if(lockedPlayers.contains(pickedPlayer)) {
+            hideAssessmentView();
+            return;
+        }
+
+        showAssessmentView();
+
         List<Player> playersToAssess = expSplittingManager.getPlayersToBeAssessedByPlayer(pickedPlayer);
 
         assessPlayersRecyclerView.setAdapter(new SurveyAdapter(this,
@@ -135,5 +157,38 @@ public class SurveyActivity extends AbstractBaseActivity implements LoadPlayersA
     @Override
     public void onBackPressed() {
         //nothing
+    }
+
+    @OnClick(R.id.lock_player_assessment)
+    public void onLockCurrentAssessmentButtonClicked() {
+        new AlertDialog.Builder(this)
+                .setTitle(R.string.lock_assessment_confirmation_title)
+                .setMessage(R.string.lock_assessment_confirmation_message)
+                .setPositiveButton(R.string.ok, new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        lockCurrentPlayerAssessment();
+                    }
+                })
+                .setNegativeButton(R.string.cancel, null)
+                .show();
+
+    }
+
+    private void lockCurrentPlayerAssessment() {
+        lockedPlayers.add(currentlyVisiblePlayer);
+        hideAssessmentView();
+    }
+
+    private void hideAssessmentView() {
+        assessPlayersRecyclerView.setVisibility(View.GONE);
+        lockAssessmentButton.setVisibility(View.GONE);
+        assessmentLockedTextContainer.setVisibility(View.VISIBLE);
+    }
+
+    private void showAssessmentView() {
+        assessPlayersRecyclerView.setVisibility(View.VISIBLE);
+        lockAssessmentButton.setVisibility(View.VISIBLE);
+        assessmentLockedTextContainer.setVisibility(View.GONE);
     }
 }
