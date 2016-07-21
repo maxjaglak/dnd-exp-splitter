c355b27fc92a0c9533640c951cb31a6028748f18
Max <maksymilian.jaglak@gmail.com>
2016-07-21 19:46:21 +0200

    adding + icon in category list, action button

--- app/src/main/java/oo/max/dndexperiencesplitter/app/Logger.java
+++ app/src/main/java/oo/max/dndexperiencesplitter/app/Logger.java
@@ -4,7 +4,7 @@ import android.util.Log;
 
 public class Logger {
 
-    public static final String TAG = "pl.supervisor.amsv";
+    public static final String TAG = "oo.max.dnd";
 
     public static void debug(String message) {
         Log.e(TAG, message);
