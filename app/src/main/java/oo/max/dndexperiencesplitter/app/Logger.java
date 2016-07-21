d92b3e7ea3ce8a90848aeed18b97050aa734e2c9
Max <maksymilian.jaglak@gmail.com>
2016-07-21 19:48:07 +0200

    logger tag update

--- app/src/main/java/oo/max/dndexperiencesplitter/app/Logger.java
+++ app/src/main/java/oo/max/dndexperiencesplitter/app/Logger.java
@@ -4,7 +4,7 @@ import android.util.Log;
 
 public class Logger {
 
-    public static final String TAG = "oo.max.dnd";
+    public static final String TAG = "oo.max.dnd.expsplitter";
 
     public static void debug(String message) {
         Log.e(TAG, message);
