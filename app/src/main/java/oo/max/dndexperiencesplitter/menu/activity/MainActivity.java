c4dbcbc58876f68bd9b2fc2462bb618870f72e33
Max <maksymilian.jaglak@gmail.com>
2016-08-04 20:56:53 +0200

    adding simple drawer layout in main activity

--- app/src/main/java/oo/max/dndexperiencesplitter/menu/activity/MainActivity.java
+++ app/src/main/java/oo/max/dndexperiencesplitter/menu/activity/MainActivity.java
@@ -2,10 +2,14 @@ package oo.max.dndexperiencesplitter.menu.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.widget.DrawerLayout;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import javax.inject.Inject;
 
+import butterknife.Bind;
 import butterknife.OnClick;
 import oo.max.dndexperiencesplitter.R;
 import oo.max.dndexperiencesplitter.category.activity.CategoryListActivity;
@@ -24,10 +28,44 @@ public class MainActivity extends AbstractBaseActivity {
     @Inject
     PlayerDao playerDao;
 
+    @Bind(R.id.drawer_view)
+    NavigationView navigationView;
+
+    @Bind(R.id.drawer_layout)
+    DrawerLayout drawerLayout;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_drawer_main);
+
+        navigationView.setNavigationItemSelectedListener(
+                new NavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(MenuItem menuItem) {
+                        selectDrawerItem(menuItem);
+                        return true;
+                    }
+                });
+    }
+
+    private void selectDrawerItem(MenuItem menuItem) {
+        switch (menuItem.getItemId()) {
+            case R.id.drawer_players:
+                startPlayersActivity();
+                break;
+            case R.id.drawer_categories:
+                startCategoriesActivity();
+                break;
+            case R.id.drawer_run_survey:
+                startExpSplitting();
+                break;
+            case R.id.drawer_history:
+                startHistoryResultsActivity();
+                break;
+        }
+
+        drawerLayout.closeDrawers();
     }
 
     @OnClick(R.id.players)
@@ -42,7 +80,7 @@ public class MainActivity extends AbstractBaseActivity {
 
     @OnClick(R.id.run)
     public void startExpSplitting() {
-        if(!canRunSurvey()) {
+        if (!canRunSurvey()) {
             showPlayersOrCategoriesMissingToast();
             return;
         }
